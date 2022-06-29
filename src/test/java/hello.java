459cb2fde2983fb74b22ac3b05060ab4db955a96
zahide84 <erozgur80@hotmail.com>
2022-06-29 18:25:16 -0400

    added more hello feature

--- src/test/java/hello.java
+++ src/test/java/hello.java
@@ -2,6 +2,8 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing hello future");
+        System.out.println("doing more work");
+        System.out.println("local repo is already published to github");
 
     }
 }
